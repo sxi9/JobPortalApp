287676b87a6b5cfa1303fb061cac30577c313a48
sxi9 <saik9737@gmail.com>
2025-07-15 23:56:02 +0530

    Update configuration for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-  },
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    base: mode === 'production' ? '/JobPortalApp/' : '/',
+    build: {
+      outDir: 'dist',
+      assetsDir: 'assets',
+      sourcemap: true
+    },
+    server: {
+      port: 5173,
+    },
+  };
 });
